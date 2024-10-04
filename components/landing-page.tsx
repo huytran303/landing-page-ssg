b5bf4e1c968e90704cf84b3c36f44cb1f946c100
TranHuy-cht <tnh.t1k30.ht@gmail.com>
2024-10-04 14:12:31 +0700

    v7.2

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -138,7 +138,7 @@ export default function EnhancedMobileFriendlyLandingPageComponent() {
             </Button>
           </div>
           <Button className="hidden md:inline-flex bg-blue-600 hover:bg-blue-700 text-white">
-            Try Now
+            Coming Soon
           </Button>
         </div>
       </header>
